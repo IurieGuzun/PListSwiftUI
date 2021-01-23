336a3bb8b2076dd414b34c0210e62d1d7c68f5ca
Iurie Guzun <iurie_guzun@yahoo.com>
2021-01-23 15:55:26 -0500

    Accessed plist

--- PListSwiftUI/ContentView.swift
+++ PListSwiftUI/ContentView.swift
@@ -7,10 +7,29 @@
 //
 
 import SwiftUI
+func getPlist(withName name: String) -> [String:Any]?
+{
+    if  let path = Bundle.main.path(forResource: name, ofType: "plist"),
+        let xml = FileManager.default.contents(atPath: path)
+    {
+        print("Inside function option 1!")
+    
+        return (try? PropertyListSerialization.propertyList(from: xml, options: .mutableContainersAndLeaves, format: nil)) as? [String:Any]
+    }
+    print("Inside function option 2!")
+    return nil
+}
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, World!")
+        Text("Hello, World!").onTapGesture {
+            print("ContentView Text Accessed!")
+            if let fruits = getPlist(withName: "PictureCoordinates") {
+                print(fruits) // Output: ["Orange", "Pineapple", "Raspberry", ···]
+            } else {
+                print("Can't read plist file!")
+            }
+        }
     }
 }
 
