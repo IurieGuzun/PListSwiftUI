a465fb2b762471afd14ac2388bad3d608123161c
Iurie Guzun <iurie_guzun@yahoo.com>
2021-01-25 11:21:05 -0500

    Plist to Dictionary

--- PListSwiftUI/ContentView.swift
+++ PListSwiftUI/ContentView.swift
@@ -7,28 +7,55 @@
 //
 
 import SwiftUI
-func getPlist(withName name: String) -> [String:Any]?
+func getPlist(withName name: String) -> [String:[String]]?
 {
     if  let path = Bundle.main.path(forResource: name, ofType: "plist"),
         let xml = FileManager.default.contents(atPath: path)
     {
         print("Inside function option 1!")
     
-        return (try? PropertyListSerialization.propertyList(from: xml, options: .mutableContainersAndLeaves, format: nil)) as? [String:Any]
+        return (try? PropertyListSerialization.propertyList(from: xml, options: .mutableContainersAndLeaves, format: nil)) as? [String:[String]]
     }
     print("Inside function option 2!")
     return nil
 }
 
 struct ContentView: View {
+    
     var body: some View {
         Text("Hello, World!").onTapGesture {
             print("ContentView Text Accessed!")
-            if let fruits = getPlist(withName: "PictureCoordinates") {
-                print(fruits) // Output: ["Orange", "Pineapple", "Raspberry", ···]
+            if let pictures = getPlist(withName: "PictureCoordinates") {
+//                print(pictures["Dove"])
+                print(pictures.keys)
+                for k in pictures.keys {
+                     print(k)
+                }
+                let ranIndex = Int.random(in: 0...pictures.count-1)
+                let index = pictures.index(pictures.startIndex, offsetBy: ranIndex)
+                
+                var  myIndex = pictures.keys[index]
+                print(index, ranIndex, myIndex, pictures.keys[index])
+                print(pictures[myIndex]!)
+                
+//                print(pictures.values)
+//                print(pictures)
+                
             } else {
                 print("Can't read plist file!")
             }
+        }.onLongPressGesture {
+            var nsDictionary: NSDictionary?
+            if let path = Bundle.main.path(forResource: "PictureCoordinates", ofType: "plist") {
+               nsDictionary = NSDictionary(contentsOfFile: path)
+                print(nsDictionary!.allKeys[0])
+                print(nsDictionary!.allKeys[1])
+                var myKey = nsDictionary!.allKeys[3]
+                print(nsDictionary!.value(forKey: myKey as! String))
+
+            var myDict = nsDictionary!.allKeys[1]
+                print(myDict)
+             }
         }
     }
 }
